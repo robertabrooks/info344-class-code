72fdad314ec06cec1afd21f64179c4ff05f288ab
Robert Brooks <robertb1@uw.edu>
2016-01-11 10:29:51 -0800

    end of lecture

--- php-forms/index.php
+++ php-forms/index.php
@@ -8,7 +8,24 @@ $appId = '2de143494c0b295cca9337e1e96b00e0';
 //weather icon URLs
 // http://openweathermap.org/img/w/{iconName}.png
 
-$q = '';
+require_once 'connection.php';
+require_once 'models/zip-model.php';
+
+$q = $_GET['q'];
+
+$conn = getConnection();
+$zipModel = new Zips($conn);
+$matches = $zipModel->search($q);
+
+if (count($matches) == 1) {
+    $zip = $matches[0]['zip'];
+    $url = "http://api.openweathermap.org/data/2.5/weather?zip={$zip},us&units=imperial&appid={$appId}";
+    $json = file_get_contents($url);
+    $weatherData = json_decode($json);
+    
+    //var_dump($zip); checks string 
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,7 +43,12 @@ $q = '';
     <?php 
     include 'views/search-form.php';
 
+    include 'views/matches.php';
+    
+    if (isset($weatherData)) {
+        include 'views/weather.php';
+    }
     ?>
-   
+
 </body>
 </html>
