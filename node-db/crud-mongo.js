5a49a65cd4973bf629f1a8d847e074b558c46859
Robert Brooks <robertb1@uw.edu>
2016-01-25 10:21:33 -0800

    class work

---
+++ node-db/crud-mongo.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+// load connection information
+var dbConfig = require('./secret/config-mongo.json');
+
+var storySchema = new mongoose.Schema({
+    url: String, 
+    votes: {type: Number, default: 0},
+    createdOn: {type: Date, default: Date.now}
+});
+
+var Story = mongoose.model('Story', storySchema);
+
+mongoose.connect(dbConfig.url);
+mongoose.connection.on('error', function(err) {
+    console.error(err);
+});
+
+var newStory = {
+    url: 'http://www.google.com'
+};
+var id;
+
+Story.create(newStory)
+    .then(function(story) {
+        id = story._id;
+        console.log('inserted new story!');
+        console.log(story);
+    })
+    // read operation
+    .then(function() {
+        return Story.findById(id).exec();        
+    })
+    .then(function(story) {
+        console.log('found story!');
+        console.log(story);
+        
+        //upadte 
+        return Story.findByIdAndUpdate(id, {$inc: {votes: 1}}, {new: true}); // increment the votes by one
+    })
+    .then(function(story) {
+        console.log('updated story!');        
+        console.log(story);
+        
+        // delete
+        return Story.findByIdAndRemove(id);
+    })
+    .then(function() {
+        console.log('story deleted!');
+    })
+    .then(null, function(err) {
+        console.error(err);
+    })
+    .then(function() {
+        mongoose.connection.close();
+    })
+    
